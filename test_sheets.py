91a9532888e740285b070aa50c287567eb2e74c7
Colonies-admin <admin@livecolonies.com>
2026-04-08 15:56:10 +0200

    test_sheets.py

--- test_sheets.py
+++ test_sheets.py
@@ -14,10 +14,8 @@ def test_connexion():
     creds = Credentials.from_service_account_info(creds_dict, scopes=SCOPES)
     client = gspread.authorize(creds)
     
-    # On ouvre par nom au lieu de l'ID
-    sheet = client.open("ORANGE SUIVI")  # remplace par le nom exact de ton sheet
-    # Juste après sheet = client.open("ORANGE SUIVI")
-print(f"ID réel du sheet : {sheet.id}")
+    sheet = client.open("ORANGE SUIVI")
+    print(f"ID réel du sheet : {sheet.id}")
     
     worksheets = sheet.worksheets()
     print("✅ Connexion réussie. Onglets trouvés :")
